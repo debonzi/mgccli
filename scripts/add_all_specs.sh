bd5c5cf1f8e479edb250b36917fb86daeb9fb77b
Daniel Debonzi <daniel.debonzi@luizalabs.com>
2025-05-05 14:47:52 -0300

    Initial files for IAM commands.

--- scripts/add_all_specs.sh
+++ scripts/add_all_specs.sh
@@ -25,5 +25,7 @@ $BASEDIR/add_specs_without_region.sh profile profile specs/conv.globaldb.openapi
 
 $BASEDIR/add_specs.sh load-balancer load-balancer specs/lbaas.openapi.yaml https://lbaas.jaxyendy.com/openapi-cli.json
 
-# EXAMPLE
+$BASEDIR/add_specs_without_region.sh iam iam specs/conv.iam.openapi.yaml https://iam.jaxyendy.com/openapi.json
+
+# EXAMPLE/
 # $BASEDIR/SCRIPT.sh NOME_NO_MENU URL_PATH LOCAL_DA_SPEC SPEC_UID
